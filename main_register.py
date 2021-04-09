45868d7803b47d8aae9ab1e6e38a89a82f507f6a
Ureshipan <tiptevlad@yandex.ru>
2021-04-09 08:58:38 +0300

    bruh

--- main_register.py
+++ main_register.py
@@ -79,8 +79,22 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
-    
-    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("http://127.0.0.1:5000/")
+        return render_template('login.html',
+                                   message="Неправильный логин или пароль",
+                                   form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
 
 def main():
     db_session.global_init("db/blogs.db")
